ab41d13e80c9743d529bc6ee2d1def542cca7c57
milakirn <kononenko.daniil2001@gmail.com>
2022-05-01 23:01:31 +0200

    Addings and barricades

--- Main.cpp
+++ Main.cpp
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 #define SDL_MAIN_HANDLED
 #include "SDL2/SDL.h"
 #include "SDL2/SDL_image.h"
 #include "Main.h"
 
-//Проект ещё будет дорабатываться :)
+//This project will be continue :)
 
 unsigned int DeltaTime(unsigned int* lastTick, unsigned int* firstTick)
 {
@@ -43,29 +44,127 @@ SDL_Texture* LoadTexture(const char* filename, int* width, int* height, SDL_Rend
 	return texture;
 }
 
+struct Vector_2
+{
+	int x;
+	int y;
+};
+
+struct Barrier
+{
+	SDL_Texture* texture;
+	Vector_2 size;
+	void Init(SDL_Texture* tex, int width, int height);
+	void Rendering(SDL_Renderer* renderer, double x, double y);
+	void Destroy();
+};
+
+void Barrier::Init(SDL_Texture* tex, int width, int height)
+{
+	texture = tex;
+	size.x = width;
+	size.y = height;
+}
+
+void Barrier::Rendering(SDL_Renderer* renderer, double x, double y)
+{
+	SDL_Rect rect;
+	rect.x = (int)round(x * 1920 / 15);
+	rect.y = (int)round(y * 1080 / 11);
+	rect.w = (int)size.x;
+	rect.h = (int)size.y;
+
+	SDL_RenderCopyEx(renderer,
+		texture,
+		nullptr,
+		&rect,
+		0,
+		nullptr,
+		SDL_FLIP_NONE);
+}
+
+void Barrier::Destroy()
+{
+	SDL_DestroyTexture(texture);
+}
+
+
+struct Player
+{
+	Barrier Barrier;
+	Vector_2 pos;
+	void Init(SDL_Texture* tex, int width, int height, int PositionX, int PositionY);
+	void Rendering(SDL_Renderer* renderer);
+	void Destroy();
+};
+
+void Player::Init(SDL_Texture* tex, int width, int height, int PositionX, int PositionY)
+{
+	Barrier.Init(tex, width, height);
+	pos.x = PositionX;
+	pos.y = PositionY;
+}
+
+void Player::Rendering(SDL_Renderer* renderer)
+{
+	Barrier.Rendering(renderer, pos.x, pos.y);
+}
+
+void Player::Destroy()
+{
+	SDL_DestroyTexture(Barrier.texture);
+}
+
+struct List
+{
+	List* PastElement;
+	unsigned char X;
+	unsigned char Y;
+};
+
+struct Stack
+{
+	List* LastElement = nullptr;
+	void AddElement(int x, int y);
+	void DeleteLastElement();
+	void Clear();
+};
+
+void Stack::AddElement(int x, int y)
+{
+	List* NewElement = (List*)malloc(sizeof(List));
+	NewElement->PastElement = LastElement;
+	NewElement->X = x;
+	NewElement->Y = y;
+	LastElement = NewElement;
+}
+
+void Stack::DeleteLastElement()
+{
+	if (LastElement)
+	{
+		List* PastElement = LastElement->PastElement;
+		free(LastElement);
+		LastElement = PastElement;
+	}
+	else
+	{
+		printf("You Delete non-existent element!");
+		abort();
+	}
+}
+
+void Stack::Clear()
+{
+	while (LastElement)
+	{
+		DeleteLastElement();
+	}
+}
+
+
 int main()
 {
-	int screenWidth = 1980;
-	int screenHeight = 1080;
-
-	float x = screenWidth / 15;
-	float y = screenHeight / 11;
-
-	int currX = x;
-	int currY = y;
-
-	unsigned int firstTick = 0;
-	unsigned int lastTick = 0;
-
-	unsigned int deltaTime = 0;
-
-	float speed = 0.5f;
-
-	const char* image_path = "tfg2.2.png";
-
-	int tex_width = 0;
-	int tex_height = 0;
-
 #pragma region InitSDL
 	// Init SDL libraries
 	SDL_SetMainReady(); // Just leave it be
@@ -84,15 +183,33 @@ int main()
 		return -1;
 	}
 
+#pragma region DisplaySize
+
+	SDL_DisplayMode display;
+
+	for (int i = 0; i < SDL_GetNumVideoDisplays(); ++i) {
+
+		int should_be_zero = SDL_GetCurrentDisplayMode(i, &display);
+
+		if (should_be_zero != 0)
+			// In case of error...
+			SDL_Log("Could not get display mode for video display #%d: %s", i, SDL_GetError());
+
+		else
+			// On success, print the current display mode.
+			SDL_Log("Display #%d: current display mode is %dx%dpx @ %dhz.", i, display.w, display.h, display.refresh_rate);
+
+	}
+#pragma endregion
+
 	// Creating the window 1920x1080 (could be any other size)
 	SDL_Window* window = SDL_CreateWindow("TheFirstGame",
 		0, 0,
-		screenWidth, screenHeight,
+		display.w, display.h,
 		SDL_WINDOW_SHOWN);
 
 	if (!window)
 		return -1;
-
 	// Creating a renderer which will draw things on the screen
 	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 	if (!renderer)
@@ -103,21 +220,55 @@ int main()
 
 #pragma endregion
 
-	SDL_Texture* texture = LoadTexture(image_path, &tex_width, &tex_height, renderer);
+	int screenWidth = display.w;
+	int screenHeight = display.h;
 
-#pragma region InitPlate
-	SDL_Surface* board;
+	int x = screenWidth / 15;
+	int y = screenHeight / 11;
 
-	board = SDL_CreateRGBSurface(0, 15, 11, 32, 0, 0, 0, 0);
+	int currX = x;
+	int currY = y;
 
-	SDL_FillRect(board, NULL, SDL_MapRGB(board->format, 129, 162, 82));
+	unsigned int firstTick = 0;
+	unsigned int lastTick = 0;
 
-	SDL_Texture* gamePlate = SDL_CreateTextureFromSurface(renderer, board);
-	SDL_FreeSurface(board);
-#pragma endregion
-	unsigned char arr[11][15];
+	unsigned int deltaTime = 0;
+
+	float speed = 0.5f;
+
+	const char* image_path = "tfg2.2.png";
+
+	int tex_width = 0;
+	int tex_height = 0;
+
+	int widthInPixels = display.w / 15;
+	int heightInPixels = display.h / 11;
+
+	unsigned char playGround[11][15];
 
 	bool done = false;
+	bool finding = false;
+	bool pathGenerate = false;
+
+	Barrier barricade;
+	Player player;
+
+	SDL_Texture* texture = LoadTexture(image_path, &tex_width, &tex_height, renderer);
+
+
+	SDL_Surface* temp;
+	temp = SDL_CreateRGBSurface(0, widthInPixels, heightInPixels, 32, 255, 0, 0, 0);
+
+	barricade.Init(SDL_CreateTextureFromSurface(renderer, temp), widthInPixels, heightInPixels);
+
+	player.Init(texture, widthInPixels, heightInPixels, 5, 6);
+	SDL_FreeSurface(temp);
+
+	Stack stack_1, stack_2;
+	Stack* checkNow = &stack_1;
+	Stack* checkNext = &stack_2;
+	Stack path;
+
 	SDL_Event sdl_event;
 
 	// The main loop
@@ -153,15 +304,41 @@ int main()
 				if (sdl_event.button.button == SDL_BUTTON_LEFT)
 				{
 					SDL_GetMouseState(&currX, &currY);
+					checkNow->Clear();
+					checkNext->Clear();
+					path.Clear();
+					checkNow->AddElement(player.pos.x, player.pos.y);
+					for (int i = 0; i < 11; i++)
+					{
+						for (int j = 0; j < 15; j++)
+						{
+							playGround[i][j] = 1;
+						}
+					}
+					playGround[player.pos.y][player.pos.x] = 2;
+					playGround[currY / heightInPixels][currX / widthInPixels] = 255;
+					finding = true;
+					break;
 				}
 			}
-			// More events here?
 		}
 
 		// Clearing the screen
 		SDL_RenderClear(renderer);
 
 		// All drawing goes here
+
+		playGround[2][7] = 0;
+		barricade.Rendering(renderer, 7, 2);
+
+		for (int i = 0; i < 5; i++)
+		{
+			int x = rand() % 15;
+			int y = rand() % 11;
+			
+			playGround[y][x] = 0;
+			barricade.Rendering(renderer, x, y);
+		}
 
 		// Let's draw a sample image
 		deltaTime = DeltaTime(&lastTick, &firstTick);
@@ -188,29 +365,20 @@ int main()
 		}
 
 
-		SDL_Rect pole;
-		pole.x = 0;
-		pole.y = 0;
-		pole.w = (int)screenWidth;
-		pole.h = (int)screenHeight;
-
-		SDL_RenderCopyEx(renderer, gamePlate, nullptr, &pole, 0, nullptr, SDL_FLIP_NONE);
-
-
 		// Here is the rectangle where the image will be on the screen
-		SDL_Rect rect;
-		rect.x = (int)round(x - tex_width / 2); // Counting from the image's center but that's up to you
-		rect.y = (int)round(y - tex_height / 2); // Counting from the image's center but that's up to you
-		rect.w = (int)tex_width;
-		rect.h = (int)tex_height;
+		//SDL_Rect rect;
+		//rect.x = (int)round(x - tex_width / 2); // Counting from the image's center but that's up to you
+		//rect.y = (int)round(y - tex_height / 2); // Counting from the image's center but that's up to you
+		//rect.w = (int)tex_width;
+		//rect.h = (int)tex_height;
 
-		SDL_RenderCopyEx(renderer, // Already know what is that
-			texture, // The image
-			nullptr, // A rectangle to crop from the original image. Since we need the whole image that can be left empty (nullptr)
-			&rect, // The destination rectangle on the screen.
-			0, // An angle in degrees for rotation
-			nullptr, // The center of the rotation (when nullptr, the rect center is taken)
-			SDL_FLIP_NONE); // We don't want to flip the image
+		//SDL_RenderCopyEx(renderer, // Already know what is that
+		//	texture, // The image
+		//	nullptr, // A rectangle to crop from the original image. Since we need the whole image that can be left empty (nullptr)
+		//	&rect, // The destination rectangle on the screen.
+		//	0, // An angle in degrees for rotation
+		//	nullptr, // The center of the rotation (when nullptr, the rect center is taken)
+		//	SDL_FLIP_NONE); // We don't want to flip the image
 
 
 // Showing the screen to the player
